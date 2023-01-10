df1ab2684737b8c0264ca48d3768f70f454bb4f4
Denis Ivanchenko <sicknature24@yandex.com>
2023-01-10 21:54:37 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = '';
+    let numberString = n.toString();
+    for (i = numberString.length - 1; i >= 0; i--) {
+        result += numberString[i];
+    }
+    return parseInt(result);
 }
